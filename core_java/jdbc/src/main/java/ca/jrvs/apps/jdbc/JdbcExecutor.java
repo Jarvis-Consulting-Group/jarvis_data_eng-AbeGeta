135b3f3af0ad3ed923feedfd5260e6d46ca2c4dc
Abraham Getachew <abegetachew99@gmail.com>
2023-04-10 19:03:16 +0000

    added files

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JdbcExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JdbcExecutor.java
@@ -1,2 +1,23 @@
-package ca.jrvs.apps.jdbc;public class JdbcExecutor {
+package ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcExecutor {
+
+    public static void main(String... args){
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+                "hplussport", "postgres", "password");
+        try {
+            Connection connection = dcm.getConnection();
+            CustomerDao customerDAO = new CustomerDao(connection);
+            Customer customer = customerDAO.findById(1000);
+            System.out.println(customer.getFirstName() + " " + customer.getLastName());
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
